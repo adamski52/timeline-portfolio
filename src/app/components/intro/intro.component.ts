7fe0b1bb4aa23e040eae5bc487cf2c49421ac9af
Jonathan Adamski <jon@Jonathans-MacBook-Pro.local>
2017-03-30 20:35:40 -0400

    warning: flashing lights.

--- src/app/components/intro/intro.component.ts
+++ src/app/components/intro/intro.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IntroComponent implements OnInit {
 
+  private _transitionMs:number = 200;
+  private _index:number = 0;
+  private _colors:string[] = ["#fc0", "#f0c", "#c0f", "#cc0", "#0fc", "#0cf"];
+
   constructor() { }
 
-  ngOnInit() {
+  getStyle() {
+    return {
+      "-webkit-transition": "all " + this._transitionMs/1000 + "s",
+      "transition": "all " + this._transitionMs/1000 + "s",
+      "background": this._colors[this._index]
+    };
   }
 
+  private getIndex():number {
+    return Math.floor(Math.random() * this._colors.length);
+  }
+
+  ngOnInit() {
+    this._index = this.getIndex();
+    let timer = setInterval(() => {
+      this._index = this.getIndex();
+    }, this._transitionMs);
+  }
 }
